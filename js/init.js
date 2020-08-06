67267b390fe67c2efafa110cf6e9b2462d4577f2
nahumoore <nahuelmoreno2904@gmail.com>
2020-08-06 13:49:09 -0300

    Entrega 1
    Pautas individuales

--- js/init.js
+++ js/init.js
@@ -15,33 +15,26 @@ var hideSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
+var getJSONData = function(PRODUCTS_URL){
     var result = {};
     showSpinner();
-    return fetch(url)
+    return fetch(PRODUCTS_URL)
     .then(response => {
       if (response.ok) {
         return response.json();
       }else{
-        throw Error(response.statusText);
+        throw Error(response.status);
       }
     })
     .then(function(response) {
           result.status = 'ok';
           result.data = response;
-          hideSpinner();
           return result;
     })
     .catch(function(error) {
         result.status = 'error';
         result.data = error;
-        hideSpinner();
         return result;
     });
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-});
