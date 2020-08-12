2caa6eaa3024772b65ca0542aa1e07a6c28ebcbd
nahumoore <nahuelmoreno2904@gmail.com>
2020-08-12 16:37:50 -0300

    Update

--- js/login.js
+++ js/login.js
@@ -2,14 +2,17 @@ var user = document.getElementById("user");
 var pass = document.getElementById("pass");
 
 function enviarForm(){
-    if (user.value===null || user.value===""){
+    if (user.value.trim()===""){
         alert("Ingresa tu usuario")
+        return false
     }
-    if (pass.value===null || pass.value===""){
+    if (pass.value.trim()===""){
         alert("Ingresa tu contraseña")
+        return false
     } 
     if (pass.value.length > 0 && user.value.length > 0){
         onclick=location.href='index2.html'
+
     }    
     }
     
